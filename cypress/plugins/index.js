5378b6ec7ceb66cbc9e80701bffb58fdff7891c6
ORicardoSouza <ricardo.vinicius@outlook.com>
2022-12-03 11:57:10 -0300

    inclusão do comando clear das pastas allure

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -15,11 +15,7 @@
 /**
  * @type {Cypress.PluginConfig}
  */
-// eslint-disable-next-line no-unused-vars
-module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
-}
+
 const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 // importa allureWriter de "@shelex/cypress-allure-plugin/writer";
 
