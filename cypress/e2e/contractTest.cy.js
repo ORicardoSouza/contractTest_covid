f7bc97ac3de50e859537acf92fa794f40aa21b24
ORicardoSouza <ricardo.vinicius@outlook.com>
2023-01-26 22:30:08 -0300

    Inclusão do plugin cypress-plugin-api

--- cypress/e2e/contractTest.cy.js
+++ cypress/e2e/contractTest.cy.js
@@ -1,18 +1,22 @@
 /// <reference types= "cypress" />
 
-
-// Pode ser usado em um arquivo de configuração de ambiente também
-const baseUrl = "https://disease.sh/"
-
-// ######################### Implementação #########################
+    // ######################### Implementação #########################
 
 describe('Testando contrato da api https://corona.lmao.ninja', () => {
     it('Testando contrato', () => {
-        cy.request('GET', `${baseUrl}v3/covid-19/countries/Brasil?yesterday=Brasil&twoDaysAgo=Brasil&strict=Brasil&allowNull=Brasil`,)
+        cy.api({
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json'
+            },
+            url: `v3/covid-19/countries/Brasil?yesterday=Brasil&twoDaysAgo=Brasil&strict=Brasil&allowNull=Brasil`,
+        })
             .then((response) => {
-                expect(response.status).to.equal(200) // Validações do status da requisição
-                expect(response.body.country).equal("Brazil") // Validações do Pais Brazil
-               cy.validateSchema('contractSchema', response.body)
+                expect(response.status)
+                    .to.equal(200) // Validações do status da requisição
+                expect(response.body.country)
+                    .equal("Brazil") // Validações do Pais Brazil
+                cy.validateSchema('contractSchema', response.body);
             })
     })
 })
